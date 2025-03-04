2c4b97e3d93991e195a26032bd814484dfcc4541
Yoon-Jemin <yjm1800@naver.com>
2025-03-04 21:38:10 +0900

    feat(gateway) 게이트웨이 모듈 추가

--- service/eureka/server/src/test/java/cafe/server/EurekaApplicationTests.java
+++ service/eureka/server/src/test/java/cafe/server/EurekaApplicationTests.java
@@ -1,10 +1,11 @@
 package cafe.server;
 
+import cafe.eureka.EurekaApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class ServerApplicationTests {
+@SpringBootTest(classes = EurekaApplication.class)
+class EurekaApplicationTests {
 
     @Test
     void contextLoads() {
