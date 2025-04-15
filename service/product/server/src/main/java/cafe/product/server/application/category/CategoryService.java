b33db1f859f82b320e710795a0e31a26fea70522
Yoon-Jemin <yjm1800@naver.com>
2025-04-15 23:22:32 +0900

    feat(product): 카테고리 수정 API 추가

--- service/product/server/src/main/java/cafe/product/server/application/category/CategoryService.java
+++ service/product/server/src/main/java/cafe/product/server/application/category/CategoryService.java
@@ -4,13 +4,19 @@ import cafe.product.server.domain.model.Category;
 import cafe.product.server.domain.repository.jpa.CategoryRepository;
 import cafe.product.server.presentation.exception.ProductErrorCode;
 import cafe.product.server.presentation.exception.ProductServerException;
+import cafe.product.server.presentation.response.CategoryResponse;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -39,5 +45,36 @@ public class CategoryService {
         return categoryRepository
                 .findByCategoryId(categoryId)
                 .orElseThrow(() -> new ProductServerException(ProductErrorCode.NOT_FOUND_CATEGORY));
+    }
+
+    @Cacheable(cacheNames = "categories-cache", key = "'categories'")
+    public List<CategoryResponse> fetchAndCacheCategories() {
+        return categoryRepository.findAllWithSubCategories().stream()
+                .filter(category -> category.getParent() == null)
+                .map(CategoryResponse::fromEntity)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    @CacheEvict(cacheNames = "categories-cache", key = "'categories'")
+    public CategoryResponse updateCategory(
+            Long targetCategoryId, String name, Long parentCategoryId) {
+        Category target = findByCategoryId(targetCategoryId);
+        Category parent =
+                Optional.ofNullable(parentCategoryId).map(this::findByCategoryId).orElse(null);
+
+        target.update(name, parent);
+        syncParentCategory(target, parent);
+        return CategoryResponse.fromEntity(target);
+    }
+
+    private void syncParentCategory(Category target, Category newParent) {
+        Category oldParent = target.getParent();
+        if (oldParent != null) {
+            oldParent.removeSubCategory(target);
+        }
+        if (newParent != null) {
+            newParent.addSubCategory(target);
+        }
     }
 }
